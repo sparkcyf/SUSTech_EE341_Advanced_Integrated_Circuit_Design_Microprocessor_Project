76b679e3427daba652afac8b4452aea975056a0f
tongy <yuant2018@mail.sustech.edu.cn>
2020-11-24 12:10:01 +0800

    create 2D array for Input and Reg

--- src/main/scala/FF8.scala
+++ src/main/scala/FF8.scala
@@ -28,22 +28,62 @@ Version: V0.1
 Date: 22/11/2020
 */
 
+import chisel3._
+
+
+/*
+Description: to store data and tag
+    FFB1    FFB2
+FFA1    FF00
+    FF01    FF10
+FFA2    FF11
+
+FFA1    FF00
+
+FFA2    FF11
+
+They are FF8block
+
+Inputs:
+  in_data: a quarter of matrices A
+Regs:
+        M * N
+  data: 2 * 8 32 Sint
+Outputs:
+  out_data: a quarter of matrices A
+Function:
+
+Author: YUAN Tong
+Version: V0.1
+Date: 22/11/2020
+*/
+
 object BLOCK_SIZE_FF8 {
-  val M_FF8 = 2
-  val N_FF8 = 8
+  val ROW_FF8 = 2
+  val COL_FF8 = 8
 }
 
-class FF8(val w: Int = 32, val m: Int = BLOCK_SIZE_FF8.M_FF8, val n: Int = BLOCK_SIZE_FF8.N_FF8) extends Module{
-  val io = IO(new Bundle{
-    val in_data = Input(Vec(m*n, SInt(w.W)))
-    val out_data = Output(Vec(m*n, SInt(w.W)))
+class FF8(val w: Int = 32, val row: Int = BLOCK_SIZE_FF8.ROW_FF8, val col: Int = BLOCK_SIZE_FF8.COL_FF8) extends Module {
+  val io = IO(new Bundle {
+    val in_data = Input(Vec(row, Vec(col, SInt(w.W))))
+    val out_data = Output(Vec(row, Vec(col, SInt(w.W))))
   })
-  
-  val data = RegInit(Vec(m*n, SInt(w.W)))
 
-  for(i <- 0 until m*n) {
-    data(i) := io.in_data(i)
-    io.out_data(i) := data(i)
+  val data = RegInit(Vec(Seq.fill(row)(Vec(Seq.fill(col)(0.S(w.W))))))
+
+  io.out_data := io.in_data
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("FF8 main function")
+    chisel3.Driver.execute(args, () => new FF8)
   }
 }
+
+//run --target-dir generated --compiler verilog
+
+
+
+
 
